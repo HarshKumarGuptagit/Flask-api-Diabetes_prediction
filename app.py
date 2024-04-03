6b2eae60207550c5bf418577136e9fd425d8fba6
Harsh Kumar Gupta <156699630+HarshKumarGuptagit@users.noreply.github.com>
2024-04-03 13:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,45 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+# import pickle
+# import numpy as np
+# import warnings
+
+# warnings.filterwarnings('ignore')
+
+# model = pickle.load(open('model.pkl', 'rb'))
+# scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "<h1>Hello World!</h1>"
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     Pregnancies = int(request.form.get('Pregnancies'))
+#     Glucose = int(request.form.get('Glucose'))
+#     BloodPressure = int(request.form.get('BloodPressure'))
+#     SkinThickness = int(request.form.get('SkinThickness'))
+#     Insulin = int(request.form.get('Insulin'))
+#     BMI = float(request.form.get('BMI'))
+#     DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+#     Age = int(request.form.get('Age'))
+
+#     input_query = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+#     input_query_std = scaler.transform(input_query)
+    
+#     result = model.predict(input_query_std)[0]
+
+#     # Serialize response data into JSON format and set Content-Type header
+#     response = jsonify({'Outcome': str(result)})
+#     response.headers.add('Content-Type', 'application/json')
+
+
+
+
+# ###################################
+
+from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 import warnings
@@ -10,10 +51,12 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 app = Flask(__name__)
 
+# Serve static HTML page
 @app.route('/')
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html')
 
+# Prediction API
 @app.route('/predict', methods=['POST'])
 def predict():
     Pregnancies = int(request.form.get('Pregnancies'))
@@ -27,14 +70,15 @@ def predict():
 
     input_query = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
     input_query_std = scaler.transform(input_query)
-    
+
     result = model.predict(input_query_std)[0]
 
-    # Serialize response data into JSON format and set Content-Type header
-    response = jsonify({'Outcome': str(result)})
-    response.headers.add('Content-Type', 'application/json')
+    return jsonify({'Outcome': str(result)})
 
-    return response
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
+#     return response
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
