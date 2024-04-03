d910bdf472d56e15c4fa1f4fde4b3291141c1cfd
Harsh Kumar Gupta <156699630+HarshKumarGuptagit@users.noreply.github.com>
2024-04-03 12:29:33 +0530

    Uploads

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,jsonify
+import pickle
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+model = pickle.load(open('model.pkl','rb'))
+scaler= pickle.load(open('scaler.pkl','rb'))
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello World!</h1>"
+
+@app.route('/predict',methods=['POST']) # without url inpit
+def predict():
+    Pregnancies = int(request.form.get('Pregnancies'))
+    Glucose = int(request.form.get('Glucose'))
+    BloodPressure = int(request.form.get('BloodPressure'))
+    SkinThickness = int(request.form.get('SkinThickness'))
+    Insulin = int(request.form.get('Insulin'))
+    BMI = float(request.form.get('BMI'))
+    DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+    Age = int(request.form.get('Age'))
+
+
+    input_query=np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    input_query_std = scaler.transform(input_query)
+    
+    result= model.predict(input_query_std)[0]
+
+    return jsonify({'Outcome':str(result)})
+
+if __name__=="__main__":
+    app.run(debug=True)
+
